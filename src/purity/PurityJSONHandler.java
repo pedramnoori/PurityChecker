c38328be1d5a3f041afad8fe99c531610a480672
Pedram Nouri <noori.pedram816@gmail.com>
2023-04-28 12:16:06 -0400

    Move and Inline validation done

--- src/purity/PurityJSONHandler.java
+++ src/purity/PurityJSONHandler.java
@@ -31,15 +31,15 @@ public class PurityJSONHandler {
 //        numberOfRefactorings("/Users/pedram/Desktop/RefactoringMiner/src/purity/Puritydata.json");
 
 
-//        String outputPath = runPurity("C:\\Users\\Pedram\\Desktop\\RefactoringMiner\\src\\purity\\MoveAndRenameMethod.json");
+        String outputPath = runPurity("/Users/pedram/Desktop/RefactoringMiner/src/purity/MoveAndInlineMethod.json");
 //
-//        calculatePrecisionAndRecallOnSpecificRefactoring("C:\\Users\\Pedram\\Desktop\\RefactoringMiner\\src\\purity\\PuritydataResultMoveAndRename.json", RefactoringType.MOVE_AND_RENAME_OPERATION);
+        calculatePrecisionAndRecallOnSpecificRefactoring("/Users/pedram/Desktop/RefactoringMiner/src/purity/PuritydataResultMoveAndInline.json", RefactoringType.EXTRACT_AND_MOVE_OPERATION);
 //        calculatePrecisionAndRecallOnSpecificRefactoring("/Users/pedram/Desktop/RefactoringMiner/src/purity/PuritydataResFeb3.json", RefactoringType.EXTRACT_OPERATION);
 //        testMethod("C:\\Users\\Pedram\\Desktop\\RefactoringMiner\\src\\purity\\PurityResultTest.json");
 //
 //        getStatistics("C:\\Users\\Pedram\\Desktop\\RefactoringMiner\\src\\purity\\PurityData.json");
 //        extractRefactoringFromOracle("/Users/pedram/Desktop/RefactoringMiner/src/purity/PurityData.json", "Move and Inline Method");
-        extractRefactoringFromOracle("/Users/pedram/Desktop/RefactoringMiner/src-test/Data/data.json", "Move and Inline Method");
+//        extractRefactoringFromOracle("/Users/pedram/Desktop/RefactoringMiner/src-test/Data/data.json", "Move and Inline Method");
 
 
     }
@@ -297,7 +297,7 @@ public class PurityJSONHandler {
                         }, 100);
             }
             objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-            String outputPath = "C:\\Users\\Pedram\\Desktop\\RefactoringMiner\\src\\purity\\PuritydataResultMoveAndRename.json";
+            String outputPath = "/Users/pedram/Desktop/RefactoringMiner/src/purity/PuritydataResultMoveAndInline.json";
             objectMapper.writeValue(new File(outputPath), arrayNode);
             return outputPath;
         } catch (IOException e) {
